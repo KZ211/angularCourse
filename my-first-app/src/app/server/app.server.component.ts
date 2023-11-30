0fdb01234a5141145015f21ad775ee850a5c4294
KZ211 <alexis.jares12@gmail.com>
2023-11-30 19:39:55 -0300

    Interpolacion entre componentes, typeScript y html

--- my-first-app/src/app/server/app.server.component.ts
+++ my-first-app/src/app/server/app.server.component.ts
@@ -5,5 +5,9 @@ import { Component } from "@angular/core";
     templateUrl: './app.server.component.html'
 })
 export class ServerComponent{
-
+    serverId = 10;
+    serverStatus = "offline";
+    getServerStatus(): string {
+        return this.serverStatus;
+    }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
 })
 export class ServersComponent implements OnInit{
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000)
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
