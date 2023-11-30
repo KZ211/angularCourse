9fbc4c23d8fb0a3c66f49ed42cdd7d2c5583d1f0
KZ211 <alexis.jares12@gmail.com>
2023-11-30 20:41:24 -0300

    Implementacio de manejo de eventos en angular

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit{
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(()=>{
@@ -16,4 +18,12 @@ export class ServersComponent implements OnInit{
     throw new Error('Method not implemented.');
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event:any){
+    console.log(event.target.value);
+    this.serverName = "My Server name is " + (<HTMLInputElement>event.target).value;
+  }
 }
